07ce45ebecdc535bd6a4e809a0522e27c7389b8a
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-10 18:18:31 +0530

    cycle052: fix tests after adversarial witness falsification

--- tests/test_cycle052_product_distinction_equality.py
+++ tests/test_cycle052_product_distinction_equality.py
@@ -1,7 +1,6 @@
 from fractions import Fraction
 from cycle052_product_distinction_equality import *
 
-
 def test_exact_slack_identity_binary_grid():
     vals=[Fraction(i,6) for i in range(7)]
     pairs=[]
@@ -13,29 +12,21 @@ def test_exact_slack_identity_binary_grid():
     for p,q in pairs:
         for r,s in pairs:
             lhs,rhs=audit_identity(p,q,r,s)
-            assert lhs==rhs
-            assert lhs>=0
+            assert lhs==rhs and lhs>=0
 
-
-def test_cross_evidence_is_strict():
-    p,q,r,s=cross_evidence_witness()
-    d1,d2=tv(p,q),tv(r,s)
-    D=tv(tensor(p,r),tensor(q,s))
+def test_strict_positive_nonidentical():
+    p,q,r,s=strict_positive_witness()
+    d1,d2=tv(p,q),tv(r,s); D=tv(tensor(p,r),tensor(q,s))
     assert D < composition_upper(d1,d2)
-    assert composition_upper(d1,d2)-D == slack_decomposition(p,q,r,s)
 
-
-def test_aligned_evidence_saturates():
-    p,q,r,s=aligned_evidence_witness()
-    d1,d2=tv(p,q),tv(r,s)
-    D=tv(tensor(p,r),tensor(q,s))
+def test_boundary_saturation():
+    p,q,r,s=boundary_saturation_witness()
+    d1,d2=tv(p,q),tv(r,s); D=tv(tensor(p,r),tensor(q,s))
     assert D == composition_upper(d1,d2)
     assert slack_decomposition(p,q,r,s)==0
 
-
-def test_degenerate_factor():
-    p=[Fraction(2,3),Fraction(1,3)]
-    q=[Fraction(1,3),Fraction(2,3)]
+def test_degenerate_identical_factor():
+    p=[Fraction(2,3),Fraction(1,3)]; q=[Fraction(1,3),Fraction(2,3)]
     r=s=[Fraction(1,2),Fraction(1,2)]
     lhs,rhs=audit_identity(p,q,r,s)
     assert lhs==rhs==0
